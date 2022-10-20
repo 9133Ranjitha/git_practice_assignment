ce5d5019dcb37dba7b3f6896d63f898178d5a4a1
Ranjitha <sranjitha053@gmail.com>
2022-10-20 22:17:09 +0530

    check the codes

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+bag="";
+    for(i=N-1;i>0;i--){
+        bag+=str[i];
+    }
+    if(bag==str){
+        console.log("Yes");
+    }else {
+        console.log("No");
+    }
---
+++ prime.js
@@ -0,0 +1,11 @@
+ var count=0;
+    for(i=1; i<=num; i++){
+        if(num%i===0){
+            count++;
+        }
+    }
+    if(count===1){
+        console.log("Yes");
+    }else{
+        console.log("No");
+        }    
